c9f27d9f6282f82fe055eaffdba2140187553e85
richardcalata <calatar6@gmail.com>
2018-04-25 11:54:59 -0500

    added backticks!

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ getMovies().then((movies) => {
     console.log('Here are all the movies:');
     movies.forEach(({title, rating, id}) => {
         console.log(`id#${id} - ${title} - rating: ${rating}`);
-        $('.movieList').append('<li>${movie.title}</li>');
+        $('.movieList').append(`<li>${title}</li>`);
         //$("table").append(`<tr><td>${id}</td><td>${title}</td><td>${rating}</td><td><button>delete</button></td></tr>`);
     });
 
