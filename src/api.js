ce75b0b20586916ccc5f5a45a5286e0ec0baea51
richardcalata <calatar6@gmail.com>
2018-04-25 15:43:35 -0500

    added ability to update JSON through user input

--- src/api.js
+++ src/api.js
@@ -1,9 +1,26 @@
 module.exports = {
-  getMovies: () => {
-    return fetch('/api/movies')
-      .then(response => response.json());
-  }
+    getMovies: () => {
+        return fetch('/api/movies')
+            .then(response => response.json());
+    },
 
+    userPost: (userPost) => {
+
+        const url = '/api/movies';
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userPost),
+        };
+        return fetch(url, options)
+            .then(results => {console.log(results)
+            })
+        //     getMovies()
+        // }
+
+    }
 };
 
 
