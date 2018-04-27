cbff2cfbdd8c6f96eadfb9f6d53f75f120dac012
richardcalata <calatar6@gmail.com>
2018-04-27 13:46:42 -0500

    Posting added

--- src/api.js
+++ src/api.js
@@ -4,23 +4,25 @@ module.exports = {
             .then(response => response.json());
     },
 
-    userPost: (userPost) => {
-
-        const url = '/api/movies';
+    addMovies: (newMovies) => {
+        const theMovies = '/api/movies';
         const options = {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(userPost),
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(newMovies)
         };
-        return fetch(url, options)
-            .then(results => {console.log(results)
-            })
-        //     getMovies()
-        // }
 
-    }
+        return fetch(theMovies, options)
+            .then(response => response.json());
+
+    },
+    movieDisplayer: (title, rating, id) =>{
+    return`<tr><td>${id}</td><td>${title}</td><td>${rating}</td><td><button class="deletebutton">Delete</button><button class="editbutton" data-id="${id}">Edit</button></td></tr> `;
+},
 };
 
-
+//
+// fetch('/api/movies').then((response)=>{
+//     const newMovie =
+//
+// })
--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@
  */
 import 'bootstrap'
 import $ from 'jquery'
-import {getMovies, userPost} from './api.js';
+import {getMovies, addMovies, movieDisplayer} from './api.js';
 // import {getMovies()} from '/src/api.js'
 //import '../movieStyle.css'
 
-$(() => {
-  $('[data-toggle="popover"]').popover()
-})
-//
+// $(() => {
+//   $('[data-toggle="popover"]').popover()
+// })
+// //
 
 
 
@@ -30,14 +30,15 @@ $(document).ready(function(){
 });
 
 // const {getMovies} = require('./api.js');
+let localMovie = [];
 
 getMovies().then((movies) => {
 
     console.log('Here are all the movies:');
     movies.forEach(({title, rating, id}) => {
+        // localMovie = movies.map();
         console.log(`id#${id} - ${title} - rating: ${rating}`);
-
-        $("table").append(`<tr><td>${id}</td><td>${title}</td><td>${rating}</td><td><button>delete</button></td></tr>`);
+        $("table").append(`<tr><td>${id}</td><td>${title}</td><td>${rating}</td><td><button class="deletebutton">Delete</button><button class="editbutton" data-id="${id}">Edit</button></td></tr> `);
     });
 
 }).catch((error) => {
@@ -45,21 +46,15 @@ getMovies().then((movies) => {
     console.log(error);
 });
 
-$("#addMV").on("click",function(){
 
-    let mTitle = $('#addMovie').val();
-    let mRating = $('#addRating').val();
-    event.preventDefault();
-    userPost({title: mTitle, rating: mRating}).then();
+$('#addMV').click((e) => {
+    e.preventDefault();
+    console.log('test');
+    const title = $('#addMovie').val();
+    const rating = $('#addRating').val();
+    // location.reload();
 
-
-    getMovies().then((movies) => {
-        $('.movieList').html('');
-
-        movies.forEach(({title, rating, id}) => {
-
-            $("table").append(`<tr><td>${id}</td><td>${title}</td><td>${rating}</td><td><button>delete</button></td></tr>`);
-        }
-        )});
-})
-
+    addMovies({title, rating}).then(movie => {
+        const movieRow = movieDisplayer(movie.title, movie.rating, movie.id);
+        $(".movieList").append(movieRow)
+    });})
